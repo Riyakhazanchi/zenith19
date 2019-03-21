ba8fc768500040d2bc27a9069e2403dd633b4fd2
Riyakhazanchi <33264154+Riyakhazanchi@users.noreply.github.com>
2019-03-21 14:49:32 +0530

    Update EventList.js

--- src/Components/EventList/EventList.js
+++ src/Components/EventList/EventList.js
@@ -13,7 +13,7 @@ const EventList = () => {
 		          <p class="code-para">Aren't you tempted by quizzes? Don't you enjoy searching answers for
 		          those tricky questions? We all go bizarre to find and submit the answers
 		          before our friends do. ‘XUNBAO' is brought to relive those gone days of
-		          excitement and fun. It was an online treasure hunt.</p>
+		          excitement and fun. It is an online treasure hunt.</p>
 		                
 		      </div>
 		    </div>
@@ -32,9 +32,9 @@ const EventList = () => {
 		    <div class="box ">
 		      <div class="code">
 		            <span class="code-head">Code Relay</span>
-			        <p class="code-para">CodeRelay is a coding contest, in which an individual or a team of
-			        two could participate. Participants were provided with some random
-			        competitive questions, to which they have to answer with 16 minutes.
+			        <p class="code-para">CodeRelay is a coding contest in which an individual or a team of
+			        two can participate. Participants will be provided with some random
+			        competitive questions to which they have to answer within 16 minutes.
 			        Lesser the time, more the marks awarded.</p>
 					                
 		      </div>
@@ -43,8 +43,8 @@ const EventList = () => {
 		      <div class="code">
 		        <span class="code-head">VR Gaming</span>
 		        <p class="code-para">Wear the head mount device (HMD) and deep dive into the virtual world.
-		        Lots of games were played there. Just at a nominal fee of Rs
-		        10/-.Imaginations came out live and illusions were fantasised.</p>
+		        Lots of games will be played just at a nominal fee of Rs
+		        10/-.Imaginations will come out live and illusions will be fantasised.</p>
 				                
 		      </div>
 		    </div>
@@ -52,8 +52,8 @@ const EventList = () => {
 		      <div class="code">
 		        <span class="code-head">Hack On</span>
 		        <p class="code-para">This event is for those who have that hacking worm in them and who
-		        craves to hack. A hacking event which provided challenges for
-		        cryptography, hacking, encryption and much more. It was an online
+		        crave to hack. A hacking event which provides challenges for
+		        cryptography, hacking, encryption and much more. It is an online
 		        pre-fest event</p>
 		                	
 		      </div>
@@ -62,20 +62,20 @@ const EventList = () => {
 		      <div class="code">
 		        <span class="code-head">AR Treasure Hunt</span>
 		        <p class="code-para">AR Treasure hunt is an event in which an individual or a team of 2 members
-		        could participate where participants tried to find hidden objects or places
-		        by following a series of clues using the Augmented Reality. The team who
-		        found all clues in minimum time won the game</p>
+		        can participate where participants try to find hidden objects or places
+		        by following a series of clues using the Augmented Reality. The team which
+		        finds all clues in minimum time wins the game</p>
 		                
 		      </div>
 		    </div>
 		    <div class="box ">
 		      <div class="code">
 		        <span class="code-head">CodeXplod</span>
-		        <p class="code-para">CodeXplod is a coding event for team of two members, It is hosted
+		        <p class="code-para">CodeXplod is a coding event for team of two members. It is hosted
 		        on Hackerearth platform in which each team is given 8 problems
-		        which they have to solve in any of programming language in given
-		        time. Each problem question comprise of 100 marks and the team
-		        which scored maximum marks in least time is declared as winner.</p>
+		        which they have to solve in any of the programming language in given
+		        time. Each problem question comprises of 100 marks and the team
+		        which scores maximum marks in least time is declared as winner.</p>
 		                
 		      </div>
 		    </div>
@@ -92,8 +92,8 @@ const EventList = () => {
 		    <div class="box ">
 		      <div class="code">
 		        <span class="code-head">Web Development</span>
-		        <p class="code-para">A Website Design competition, in which an individual or a team of
-		        two can participate.In this event participants have to design the
+		        <p class="code-para">A Website Design competition in which an individual or a team of
+		        two can participate. In this event participants have to design the
 		        website on a particular theme and Internet usage is allowed. Crosscollege participation is allowed i.e both participants can be from different colleges.
 		        </p>
 		                
